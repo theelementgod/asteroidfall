e2e9c074e65e311b67ced0f0e3b834eeb7de0b42
theelementgod <theelementgod@gmail.com>
2023-12-15 11:29:53 -0700

    2nd commit

--- asteroidfall.py
+++ asteroidfall.py
@@ -29,42 +29,42 @@ while not game_over:
         if event.type == pygame.QUIT:
             game_over = True
 
-    if speed_clock%50==0:
-        asteroid_speed += 1
+        if speed_clock%50==0:
+            asteroid_speed += 1
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        astronaut_pos[0] -= 10
-        if facing_right:
-            astronaut = pygame.transorm.flip(astronaut, True, False)
-            facing_right = False
-    elif keys[pygame.K_RIGHT]:
-        asteroid_pos[0] += 10
-        if not facing_right:
-            asteroid = pygame.transorm.flip(astronaut, True, False)
-            facing_right = True
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            astronaut_pos[0] -= 10
+            if facing_right:
+                astronaut = pygame.transform.flip(astronaut, True, False)
+                facing_right = False
+        elif keys[pygame.K_RIGHT]:
+            astronaut_pos[0] += 10
+            if not facing_right:
+                astronaut = pygame.transform.flip(astronaut, True, False)
+                facing_right = True
 
-    astronaut_pos[0] = max(0, min(asteroid_pos[0], screen_width - astronaut_size[0]))
+        astronaut_pos[0] = max(0, min(asteroid_pos[0], screen_width - astronaut_size[0]))
 
-    asteroid_pos[1] += asteroid_speed
+        asteroid_pos[1] += asteroid_speed
 
-    asteroid_pos[1] += asteroid_speed
-    if asteroid_pos[1] > screen_height:
-        asteroid_pos = [random.randint(0, screen_width - asteroid_size[1])]
+        asteroid_pos[1] += asteroid_speed
+        if asteroid_pos[1] > screen_height:
+            asteroid_pos = [random.randint(0, screen_width - asteroid_size[1])]
 
-    astronaut_rect = pygame.Rect(astronaut_pos[0], astronaut_pos[1], astronaut_size[0], astronaut_size[1])
-    asteroid_rect = pygame.Rect(asteroid_pos[0], asteroid_pos[1], asteroid_size[0], asteroid_size[1])
-    if astronaut_rect.colliderect(asteroid_rect):
-        game_over = True
+        astronaut_rect = pygame.Rect(astronaut_pos[0], astronaut_pos[1], astronaut_size[0], astronaut_size[1])
+        asteroid_rect = pygame.Rect(asteroid_pos[0], asteroid_pos[1], asteroid_size[0], asteroid_size[1])
+        if astronaut_rect.colliderect(asteroid_rect):
+            game_over = True
 
-    screen.fill((0,0,0))
-    screen.blit(astronaut, (astronaut_pos[0], astronaut_pos[1]))
-    screen.blit(asteroid, (asteroid_pos[0], asteroid_pos[1]))
-    pygame.display.update()
+        screen.fill((0,0,0))
+        screen.blit(astronaut, (astronaut_pos[0], astronaut_pos[1]))
+        screen.blit(asteroid, (asteroid_pos[0], asteroid_pos[1]))
+        pygame.display.update()
 
-    speed_clock += 1
+        speed_clock += 1
 
-    clock.tick(30)
+        clock.tick(30)
 
 pygame.quit()
 
